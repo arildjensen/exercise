484d46750b0c593a11ea6fc451f4d2c1d9cdedd3
Arild Jensen <ajensen@counter-attack.com>
2023-12-21 09:55:58 +0100

    Updated readme, added comments to script

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# exercise
+# Exercise
+
+## Overview
+This repository contains the code of an exercise to build a script to collect metrics from a running Docker container and write the data to a local file in either json or cvs format at a specified frequency.
+
+## Files
+* run.py -- the main script
+* requirements.txt -- PIP packages required
+* config.json -- Configuration settings used by the main script
+* Dockerfile -- Used to create a custom nginx image
+* README.md -- This file
+* metrics -- Automatically created by the script. This is the default output file name and can be changed
+
+## Installation
+It is assumed you have Python 3 and Docker Desktop already installed.
+
+Add the required packaged
+```sh
+pip -r requirements.txt
+```
+
+Create the Docker image
+```sh
+cd ~/git/exercise
+docker build -t mynginx .
+```
+
+Start the Docker container using Docker Desktop
+
+Start the script
+```sh
+cd ~/git/exercise
+python3 run.py
+```
+
+## Future Improvements
+* The csv output was not implemented but can easily be done given some more time
+* A bug was encountered during development where the Docker API stopped working silently in the script during execution but always started working again with a new terminal session. This needs to be investigated further.
+* Better input sanitation, such as no empty input or special characters.
+* Some code cleanup for readability, variable management, etc.
+* More more functions to limit repeat code blocks
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import docker
 import json
+import csv
 import multiprocessing
 import time
 
@@ -12,13 +13,16 @@ run_freq       = (config_data['initial_freq'])
 output_file    = (config_data['output_file'])
 file_format    = (config_data['format'])
 
+# Create a background task that will query the container metrics and write output to a file
 def background_task(pause_event):
     # Connect to Docker API and get stats, wait, then repeat
     
     while True:
         if pause_event.is_set():
+            # If we are paused, do nothing
             pause_event.wait()
         else:
+            # If we are NOT paused, read config settings
             f = open('config.json')
             config_data = json.load(f)
             f.close()
@@ -26,12 +30,15 @@ def background_task(pause_event):
             local_container_name = (config_data['container_name'])
             local_output_file    = (config_data['output_file'])
             local_format         = (config_data['format'])
+            # Get metrics from running Docker container
             client = docker.from_env()
             container = client.containers.get(local_container_name)
             stats = container.stats(decode=None, stream = False)
+            # Write to output file
             with open(local_output_file, 'a') as json_file:
                 json.dump(stats, json_file, indent=2)
             client.close()
+            # Pause a set time until we repeat what we just did
             time.sleep(local_run_freq)
 
 if __name__ == "__main__":
@@ -51,21 +58,26 @@ if __name__ == "__main__":
         print(" stop : stops the monitoring process")
         print(" freq : sets monitoring frequency")
         print(" format : switches between json and csv")
+        print(" output : set the name of the output file")
         print(" exit : exits this program")
         user_input = input("> ")
         if user_input == "start":
+            # Unpause the background process and start collecting metrics
             pause_event.clear()
             print("start")
         elif user_input == "stop":
+            # Stop the background process and the collection of metrics
             pause_event.set()
             print("stop")
         elif user_input == "freq":
+            # Set the collection frequency and write to the config file
             print("Current frequency is: " + str(run_freq) + " seconds")
             run_freq = input("Please enter new frequency: ")
             config_data['initial_freq'] = int(run_freq)
             with open('config.json', 'w') as json_file:
                 json.dump(config_data, json_file, indent=2)
         elif user_input == "format":
+            # Set the output format and write to the config file
             print("Current format is " + file_format)
             print(file_format)
             file_format = input("Please enter new format (json or csv): ")
@@ -81,7 +93,15 @@ if __name__ == "__main__":
             config_data['format'] = file_format
             with open('config.json','w') as json_file:
                 json.dump(config_data, json_file, indent=2)
+        elif user_input == "output":
+            # Set the output file name and write to the config file
+            print("Current output is " + output_file)
+            output_file = input("Pleasae enter new output file name: ")
+            config_data['output_file'] = output_file
+            with open('config.json','w') as json_file:
+                json.dump(config_data, json_file, indent=2)
         elif user_input == "exit":
+            # Stop the background process and exit this script
             background_process.terminate()
             break
         else:
